0c627164e5128f3fe5f1475963cba695fb8d81a8
khaoula93atig <61363042+khaoula93atig@users.noreply.github.com>
2021-05-17 16:49:48 +0100

    route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProduitsComponent } from './produit/produits/produits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produit', component: ProduitsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
