ddf573a675bc151088fb903ffea4bb9c18d12f0c
khaoula93atig <61363042+khaoula93atig@users.noreply.github.com>
2021-05-17 18:34:27 +0100

    composant modif4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './authentification/auth/auth.component';
+import { BoutiquesComponent } from './boutique/boutiques/boutiques.component';
+import { ComplaintComponent } from './complaint/complaint/complaint.component';
+import { HeaderComponent } from './composants/header/header.component';
+import { TeamComponent } from './composants/team/team.component';
 import { ProduitsComponent } from './produit/produits/produits.component';
 
 const routes: Routes = [
-  {path: 'produit', component: ProduitsComponent}
+  {path: '', component: HeaderComponent},
+  {path: 'produit', component: ProduitsComponent},
+  {path: 'boutique', component: BoutiquesComponent},
+  {path: 'apropos', component: TeamComponent},
+  {path: 'login', component: AuthComponent},
+  {path: 'service', component: ComplaintComponent}
 ];
 
 @NgModule({
