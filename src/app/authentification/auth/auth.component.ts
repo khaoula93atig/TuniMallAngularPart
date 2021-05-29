7f85e6668d4974c1b1210dcf4c75f5b2c63b5497
khaoula93atig <61363042+khaoula93atig@users.noreply.github.com>
2021-05-29 19:39:48 +0100

    auth

--- src/app/authentification/auth/auth.component.ts
+++ src/app/authentification/auth/auth.component.ts
@@ -1,19 +1,64 @@
+
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+//import { ToastrService } from 'ngx-toastr';
+import {TokenService} from '../token.service';
+import {AccountService} from '../account.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = new FormGroup({
+    email: new FormControl(null, [Validators.required]),
+    encryptedPassword: new FormControl(null, [Validators.required, Validators.minLength(8)])
+  });
+
+  constructor(
+    private authService: AuthService,
+    private token: TokenService,
+    private account: AccountService,
+    private router: Router,
+    //private toastr:
+  ) { }
 
   ngOnInit(): void {
   }
-  processForm(monFormulaire: NgForm) {
-    console.log(monFormulaire);
+
+  signIn() {
+    console.log(this.loginForm.value)
+    this.authService.login(this.loginForm.value)
+      .subscribe(
+        res => this.handleResponse(res),
+        /*err => this.toastr.error
+        (
+          `Erreur`,
+          'Merci de Vérifier votre email ou mot de passe !',
+          {
+            timeOut: 3000,
+            positionClass: 'toast-bottom-left'
+          }
+        )*/)
+  }
+
+  handleResponse(data) {
+    this.token.handle(data);
+    this.account.changeAuthStatus(true);
+    /*this.toastr.success(
+      `Bienvenu : ${ this.token.getInfos().name }`,
+      'Vous êtes connectés !',
+      {
+        timeOut: 3000,
+        positionClass: 'toast-bottom-left'
+      }
+    )*/;
+    this.router.navigateByUrl('/');
   }
 
 }
+
