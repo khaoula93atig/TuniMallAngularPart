ea733baf3de15c0475960dbec0d7a029c6f167ac
malekannabi <60436343+malek-annabi@users.noreply.github.com>
2021-06-03 15:58:40 +0200

    Revert "Malek test"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,5 @@
-import { HomeComponent } from './home/home.component';
-import { CommandeComponent } from './commande/commande/commande.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Command } from 'protractor';
 import { AuthComponent } from './authentification/auth/auth.component';
 import { BoutiquesComponent } from './boutique/boutiques/boutiques.component';
 import { ComplaintComponent } from './complaint/complaint/complaint.component';
@@ -11,15 +8,13 @@ import { TeamComponent } from './composants/team/team.component';
 import { ProduitsComponent } from './produit/produits/produits.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HeaderComponent},
   {path: 'produit', component: ProduitsComponent},
   {path: 'boutique', component: BoutiquesComponent},
   {path: 'apropos', component: TeamComponent},
   {path: 'login', component: AuthComponent},
-
-  {path: 'service', component: ComplaintComponent},
-  {path: 'commande',component: CommandeComponent}
   {path: 'service', component: ComplaintComponent}
+
 ];
 
 @NgModule({
